6de83c9e088f09d86c6fb54f2482f59dd17618a3
user <user@user-PC>
2018-11-24 17:34:01 +0900

    ArrayUtil 완성

--- src/chapter2/ArrayUtil.java
+++ src/chapter2/ArrayUtil.java
@@ -2,15 +2,34 @@
 
 public class ArrayUtil {
 	public static int[] doubleToInt(double[] d) {
-		return null;
+		int[] result = new int[d.length];
+		for(int i = 0; i < d.length; i++) {
+			result[i] = (int)d[i];
+		}
+		return result;
 	}
 
 	public static double[] intToDouble(int[] i) {
-		return null;
+		double[] result = new double[i.length];
+		for(int j = 0; j < i.length; j++) {
+			result[j] = i[j];
+		}
+		return result;
 	}
 	
 	public static int[] concat(int[] i, int[] j) {
-		return null;
+		int[] result = new int[i.length + j.length];
+		
+		int index = 0;
+		
+		for(int val : i) {
+			result[index++] = val;
+		}
+
+		for(int val : j) {
+			result[index++] = val;
+		}
+		
+		return result;
 	}
-	
 }
